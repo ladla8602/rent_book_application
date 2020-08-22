e548fa0f02f438ff2455247b94359776fa6a8b63
unknown <gayasuddin.mohd@fxbytes.com>
2020-08-22 14:07:46 +0530

    nx-admin setup

---
+++ frontend/src/app/@core/utils/index.ts
@@ -0,0 +1,5 @@
+export { LayoutService } from './layout.service';
+export { AnalyticsService } from './analytics.service';
+export { PlayerService } from './player.service';
+export { StateService } from './state.service';
+export { SeoService } from './seo.service';
